d5ee9966090de44e17eb4f604072f8e715ba5f68
abraham <apolichenco@gmail.com>
2022-06-08 15:00:25 -0400

    commit tree 6e382d0fabded804c81c62a320db06b65f5674f8

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    const thankYouMessages = [];
+    for (let a = 0; a < name.length; a++) {
+        thankYouMessages.push(`Thank you, ${name[a]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+}
+
+
+const gifts = ["teddy bear", "drone", "doll",];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function countDown(number) {
+   let b = 10;
+    while (b >= 0) {
+        console.log(b--)
+    } 
+}
